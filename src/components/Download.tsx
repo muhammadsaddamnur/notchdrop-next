e2a28daea57fbed05c5e1b263a9e4d5c69d44881
Muhammad Saddam Nur <muhammadsaddamnur@gmail.com>
2026-01-12 08:39:17 +0700

    R2 download

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -55,7 +55,7 @@ export default function Download() {
 
           <div className="flex flex-col sm:flex-row gap-4 justify-center">
             <a
-              href="https://drive.google.com/drive/folders/1TQ-qOLHSZDiIFVvXBe9TxlDrEOA8tN_n?usp=sharing"
+              href="https://cdn.notchdrop.com/NotchDrop.dmg"
               className="inline-flex items-center justify-center gap-3 px-8 py-4 text-lg font-medium bg-white text-black rounded-xl hover:bg-white/90 transition-all hover:scale-105"
             >
               <svg
